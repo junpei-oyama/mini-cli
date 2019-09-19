668a9196ef71932fb7f35fe5ff36f0b42180ab4d
junpei-oyama <junpei.oyama.ai@gmail.com>
2019-09-19 14:41:41 +0900

    divmod()を使ってリファクタリングをした

--- warikan.py
+++ warikan.py
@@ -1,10 +1,8 @@
 def main():
-
     payment = int(input("合計金額："))
     number_of_people = int(input("人数："))
 
-    payment_1 = payment // number_of_people
-    remainder = payment % number_of_people
+    payment_1, remainder = divmod(payment, number_of_people)
 
     print(f"{payment}円 {number_of_people}人 １人あたり{payment_1}円です。端数は{remainder}円です。")
 
